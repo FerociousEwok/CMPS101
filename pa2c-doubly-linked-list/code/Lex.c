275e0d156fb8cfb63b428954c5a6551a5e4668cb
Brad Bernard <bradbernard@me.com>
2015-07-06 16:04:33 -0700

    Initial PA2

---
+++ pa2c-doubly-linked-list/code/Lex.c
@@ -0,0 +1,64 @@
+// $Id: Lex.c,v 1.1 2015-07-06 15:11:46-07 - - $
+// Bradley Bernard, bmbernar@ucsc.edu
+// CS 101: PA2
+// July 6, 2015
+//
+// Lex.c
+// Sorts the input file using a List ADT and writing
+// to an output file.
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAX_LEN 160
+
+int main(int argc, char * argv[]){
+
+   int n, count=0;
+   FILE *in, *out;
+   char line[MAX_LEN];
+   char tokenlist[MAX_LEN];
+   char* token;
+
+   // check command line for correct number of arguments
+   if( argc != 3 ){
+      printf("Usage: %s <input file> <output file>\n", argv[0]);
+      exit(1);
+   }
+
+   // open files for reading and writing 
+   in = fopen(argv[1], "r");
+   out = fopen(argv[2], "w");
+   if( in==NULL ){
+      printf("Unable to open file %s for reading\n", argv[1]);
+      exit(1);
+   }
+   if( out==NULL ){
+      printf("Unable to open file %s for writing\n", argv[2]);
+      exit(1);
+   }
+
+   /* read each line of input file, then count and print tokens */
+   while( fgets(line, MAX_LEN, in) != NULL)  {
+      count++;
+      n = 0;
+      token = strtok(line, " \n");
+      tokenlist[0] = '\0';
+      while( token!=NULL ){
+         strcat(tokenlist, "   ");
+         strcat(tokenlist, token);
+         strcat(tokenlist, "\n");
+         n++;
+         token = strtok(NULL, " \n");
+      }
+      fprintf(out, "line %d contains %d token%s: \n", count, n, n==1?"":"s");
+      fprintf(out, "%s\n", tokenlist);
+   }
+
+   /* close files */
+   fclose(in);
+   fclose(out);
+
+   return(0);
+}
